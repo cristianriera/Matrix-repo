b1f87fb5d09b7a88e9c86dba1a6867ef5306de5e
cristianriera <7criera@stpatrick.org>
2017-01-20 10:48:14 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Matrix-repo
+# Matrix-Activities
+See Showbie
---
+++ power.js
@@ -0,0 +1,19 @@
+const Matrix = require("matrix-js");
+
+let A = Matrix([
+    [0,1,1,1],
+    [1,0,1,0],
+    [1,1,0,0],
+    [1,0,0,0]
+]);
+
+function power(mtrx,p){
+    //mtrx is the matrix you input
+    //p is the power of the matrix you want
+    while()
+      let squared = matrix
+}
+
+
+//testing code
+console.log(power(A,10));
